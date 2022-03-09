27073545fabeac365e8ab63470388c16ff873380
Iury <abc@email.com>
2022-03-08 21:59:10 -0400

    estilizacao quase finalizada

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-import './style.css'
+//import './style.css'
+
+//$('task-description').val()
+const $ = document.querySelector.bind(document)
+
+const addTask =(event)=>{
+  event.preventDefault()
+  const taskDescription= $('#task-description').value
+
+  const newTaskitem= `
+   <li>${taskDescription}</li>
+  `
+
+  $('#task-list').insertAdjacentHTML('beforeend', newTaskitem)
+ // $('#task-description').value= ''
+ $('#task-form').reset()
+}
+
